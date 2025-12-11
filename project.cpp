cebe2afa4881b5e77aeaf27fe7b83b2e6509cfa3
Ali Kazim <154244144+ali-kazim@users.noreply.github.com>
2025-12-11 14:33:12 +0100

    Initial version

---
+++ project.cpp
@@ -0,0 +1,38 @@
+$PARAM
+TVCL   = 7.95,    // L/h
+TVV    = 190,   // L 
+TVka   = 0.17,  // 1/h
+
+$CMT DEPOT CENT // 1-compartmental model with absorption
+
+$INPUT BW = 38 // kg, covariate which has effect on PK parameters
+
+$MAIN
+double BWEffCL = 0.75    // BW effect on CL
+double BWEffV  = 1       // BW effect on V
+double BWEffka = -(0.25) // BW effect on ka
+
+double CL  = TVCL * pow((BW/70),BWEffCL) * exp(ETA(1)); // power function for scaling
+double V   = TVV  * pow((BW/70),BWEffV)               ;
+double ka  = TVka * pow((BW/70),BWEffka)              ;
+
+double k10 = CL/V ; 
+
+$OMEGA
+0.1415863641 // CV_CL = 39%
+
+$SIGMA
+0.0974896213 // EPS(1)
+0.0324       // EPS(2)
+
+$ODE
+dxdt_DEPOT   = -(ka) * DEPOT                 ;
+dxdt_CENT    =  (ka) * DEPOT  - (k10) * CENT ;
+
+$TABLE 
+double IPRED = CENT/V                        ; 
+double DV    = IPRED * (1+EPS(1)) + EPS(2)   ;
+double BWsim = BW                            ;
+
+$CAPTURE
+IPRED DV BWsim
